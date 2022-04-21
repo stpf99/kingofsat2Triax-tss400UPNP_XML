f532a9a4a8903ee5a296745987cd234cf71a0ef6
devegoo <57900186+devegoo@users.noreply.github.com>
2022-04-21 18:52:29 +0200

    Update getchannels.sh

--- getchannels.sh
+++ getchannels.sh
@@ -7,22 +7,25 @@ SERVER="192.168.1.114"
 # cleanup old files
 rm *.php
 rm *.xml
-
+rm -r pos/
 # download PHP files from kingofsat
-#wget https://de.kingofsat.net/pos-19.2E.php
-#wget https://de.kingofsat.net/pos-28.2E.php
-#wget https://de.kingofsat.net/pos-23.5E.php
+wget https://de.kingofsat.net/pos-19.2E.php
+wget https://de.kingofsat.net/pos-28.2E.php
+wget https://de.kingofsat.net/pos-23.5E.php
 wget https://de.kingofsat.net/pos-13E.php
 
 # call python script to extract channel information from php files and generate m3u files
 # the number at the end of the call is the satip src parameter (diseqc position)
-#python getchannels.py $SERVER pos-19.2E.php 1
-#python getchannels.py $SERVER pos-28.2E.php 2
-#python getchannels.py $SERVER pos-23.5E.php 3
-python getchannels.py $SERVER pos-13E.php 1
+python getchannels.py $SERVER pos-19.2E.php 1
+python getchannels.py $SERVER pos-28.2E.php 2
+python getchannels.py $SERVER pos-23.5E.php 3
+python getchannels.py $SERVER pos-13E.php 4
 
 # merge all xml files together
-echo "<?xml version="1.0" encoding="UTF-8"?><channelTable msys="DVB-S">" >> allChannels.xml
 cat *.xml >> allChannels.xml
-echo "</channelTable>" >> allChannels.xml
+mkdir pos
+touch echo *.xml pos/
+echo "<?xml version="1.0" encoding="UTF-8"?><channelTable msys="DVB-S">" > pos/*.xml
+echo *.xml pos/*.xml
+echo "</channelTable>" >> pos/*.xml
 
