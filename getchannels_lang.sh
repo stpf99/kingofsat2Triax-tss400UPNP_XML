98c43fbaeae8aace9522d4cf9a230c53ea734149
devegoo <57900186+devegoo@users.noreply.github.com>
2022-04-23 16:20:09 +0200

    Add files via upload

--- getchannels_lang.sh
+++ getchannels_lang.sh
@@ -9,31 +9,39 @@ mkdir 1 2 3 4
 rm *.php
 rm *.xml
 rm /tmp/*.xml
-P1=13.0E
+P1=13E
 P2=19.2E
 P3=23.5E
 P4=28.2E
-#tv-13.0E-fta.php
 declare -a P
 P=($P1 $P2 $P3 $P4 )
 
+L1=pol
+L2=ger
+L3=ger
+L4=eng
+declare -a L
+L=($L1 $L2 $L3 $L4 )
+
 # download PHP files from kingofsat
 cd 1
-#wget https://de.kingofsat.net/tv-$P1-fta.php
-wget "https://de.kingofsat.net/freqs.php?&pos=13E&standard=All&ordre=freq&filtre=Clear&cl=pol"
-mv 'freqs.php?&pos=13E&standard=All&ordre=freq&filtre=Clear&cl=pol' tv-$P1-fta.php
+wget "https://de.kingofsat.net/freqs.php?&pos=$P1&standard=All&ordre=freq&filtre=Clear&cl=$L1"
+mv "freqs.php?&pos=$P1&standard=All&ordre=freq&filtre=Clear&cl=$L1" tv-$P1-fta.php
 python ../getchannels.py $SERVER tv-$P1-fta.php 1
 cd ..
 cd 2
-wget https://de.kingofsat.net/tv-$P2-fta.php
+wget "https://de.kingofsat.net/freqs.php?&pos=$P2&standard=All&ordre=freq&filtre=Clear&cl=$L2"
+mv "freqs.php?&pos=$P2&standard=All&ordre=freq&filtre=Clear&cl=$L2" tv-$P2-fta.php
 python ../getchannels.py $SERVER tv-$P2-fta.php 2
 cd ..
 cd 3
-wget https://de.kingofsat.net/tv-$P3-fta.php
+wget "https://de.kingofsat.net/freqs.php?&pos=$P3&standard=All&ordre=freq&filtre=Clear&cl=$L3"
+mv "freqs.php?&pos=$P3&standard=All&ordre=freq&filtre=Clear&cl=$L3" tv-$P3-fta.php
 python ../getchannels.py $SERVER tv-$P3-fta.php 3
 cd ..
 cd 4
-wget https://de.kingofsat.net/tv-$P4-fta.php
+wget "https://de.kingofsat.net/freqs.php?&pos=$P4&standard=All&ordre=freq&filtre=Clear&cl=$L4"
+mv "freqs.php?&pos=$P4&standard=All&ordre=freq&filtre=Clear&cl=$L4" tv-$P4-fta.php
 python ../getchannels.py $SERVER tv-$P4-fta.php 4
 cd ..
 
