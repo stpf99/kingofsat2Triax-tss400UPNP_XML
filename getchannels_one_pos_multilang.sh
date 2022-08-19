f4ff6c203ee5d4e8bdf99209aa808e9f2c019d04
Tomasz Świątek <57900186+devegoo@users.noreply.github.com>
2022-08-19 21:30:47 +0200

    Add files via upload

--- getchannels_one_pos_multilang.sh
+++ getchannels_one_pos_multilang.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # configure SATIP server name/IP address, here
 SERVER="192.168.178.42"
+# cleanup old files
+rm *.php *.xml freq*
 P=13E
 L1=pol
 L2=ger
@@ -10,17 +12,16 @@ declare -a L
 L=($L1 $L2 $L3 $L4 )
 STR2='<?xml version="1.0" encoding="UTF-8"?><channelTable msys="DVB-S">'
 STR3='</channelTable>'
-echo $STR2 > AllChannels.xml
+echo $STR2 > TV-$P-FTA-langs.xml
 for i in "${L[@]}";
 do
 wget "https://de.kingofsat.net/freqs.php?&pos=$P&standard=All&ordre=freq&filtre=Clear&cl=$i"
-mv "freqs.php?&pos=$P&standard=All&ordre=freq&filtre=Clear&cl=$i" tv-$P-fta-$i.php
-python getchannels.py $SERVER tv-$P-fta-$i.php 1
-cat tv-$P-fta-$i.xml | awk '{ sub(/NR/, ++i) } 1' |  sed 's/&//g' | sed 's/>V</>v</g' | sed 's/>H</>h</g' >> TV-$P-FTA-$i.xml
+cat freqs*  >> tv-$P-fta-langs.php
 done
-cat TV-$P-FTA-*.xml >> AllChannels.xml
-echo $STR3 >> AllChannels.xml
+python getchannels.py $SERVER tv-$P-fta-langs.php 1
+cat tv-$P-fta-langs.xml | awk '{ sub(/NR/, ++i) } 1' |  sed 's/&//g' | sed 's/>V</>v</g' | sed 's/>H</>h</g' >> TV-$P-FTA-langs.xml
+echo $STR3 >> TV-$P-FTA-langs.xml
 
 mkdir ONEPOSMULTILANG/
-mv AllChannels.xml ONEPOSMULTILANG/
+mv TV-$P-FTA-langs.xml ONEPOSMULTILANG/
 rm *.php *.xml freq*
